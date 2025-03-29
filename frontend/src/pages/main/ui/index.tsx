dfb7453f8ef9fb6e7a7174c7b0fd8416fe34fc57
AliaksandrHrychonak <@icloud.com>
2025-03-29 04:24:28 +0300

    feat: add main page

--- frontend/src/pages/main/ui/index.tsx
+++ frontend/src/pages/main/ui/index.tsx
@@ -1,11 +1,7 @@
 import type { FC, JSX } from 'react';
 
 export const MainPage: FC = (): JSX.Element => {
-    return (
-            <main className='grid pt-20 pr-20 pl-20 max-[900px]:pr-5 max-[900px]:pl-5'>
-                main
-            </main>
-    );
+    return <div>main</div>;
 };
 
 export default MainPage;
