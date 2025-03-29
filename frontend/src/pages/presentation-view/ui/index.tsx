10bac8208eb5bd72199440dc693e5eae963d2091
AliaksandrHrychonak <@icloud.com>
2025-03-29 04:22:28 +0300

    feat(presentation): add ui presentation view with faker data

---
+++ frontend/src/pages/presentation-view/ui/index.tsx
@@ -0,0 +1,7 @@
+import { PresentationView } from '@widgets/presentation-view';
+
+import type { JSX } from 'react';
+
+export const PresentationViewPage = (): JSX.Element => {
+    return <PresentationView />;
+};
