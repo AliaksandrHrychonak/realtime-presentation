96218fd0942331edc8a621dbd4ca5cdf49df646f
AliaksandrHrychonak <@icloud.com>
2025-03-29 04:23:10 +0300

    feat(presentation): add presentation list page

---
+++ frontend/src/pages/dashboard-presentation-list/ui/index.tsx
@@ -0,0 +1,7 @@
+import { PresentationList } from '@widgets/presentation-list';
+
+import type { JSX } from 'react';
+
+export const DashboardPresentationListPage = (): JSX.Element => {
+    return <PresentationList />;
+};
