0a8053984c0bffd1728f734f3f23d3c552983302
AliaksandrHrychonak <@icloud.com>
2025-03-30 20:43:50 +0300

    build: fix build async err

--- frontend/src/pages/dashboard-presentation-profile/ui/index.tsx
+++ frontend/src/pages/dashboard-presentation-profile/ui/index.tsx
@@ -3,12 +3,13 @@ import { PresentationProfile } from '@widgets/presentation-profile';
 import type { FC, JSX } from 'react';
 
 interface IDashboardPresentationProfileProps {
-    params: {
+    params: Promise<{
         id: string;
-    };
+    }>;
 }
+
 export const DashboardPresentationProfile: FC<IDashboardPresentationProfileProps> = async ({
     params,
 }): Promise<JSX.Element> => {
-    return <PresentationProfile presentationId={params.id} />;
+    return <PresentationProfile presentationId={(await params).id} />;
 };
--- frontend/src/pages/presentation-view/ui/index.tsx
+++ frontend/src/pages/presentation-view/ui/index.tsx
@@ -3,11 +3,11 @@ import { PresentationView } from '@widgets/presentation-view';
 import type { FC, JSX } from 'react';
 
 interface PresentationPageProps {
-    params: {
+    params: Promise<{
         id: string;
-    };
+    }>;
 }
 
 export const PresentationPage: FC<PresentationPageProps> = async ({ params }): Promise<JSX.Element> => {
-    return <PresentationView presentationId={params.id} />;
+    return <PresentationView presentationId={(await params).id} />;
 };
